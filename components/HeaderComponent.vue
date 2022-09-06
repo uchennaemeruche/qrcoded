42796b5b7f3302af76fb8863a9bf260d06c21a87
Uchenna Emeruche <emerucheuchenna@gmail.com>
2022-09-06 14:01:26 +0100

    first commit

---
+++ components/HeaderComponent.vue
@@ -0,0 +1,5 @@
+<template lang="">
+    <div>
+        Component
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+
+<template>
+    <div>
+        <h1>Hello There</h1>
+        <HeaderComponent/>
+    </div>
+</template>
